6fce97d09a61160beb55e127d79506e64799e861
shemer binner <shemer146@gmail.com>
2022-03-02 23:15:47 +0200

    trying to handle the grid

--- js/apps/keep/cmps/note-img.cmp.js
+++ js/apps/keep/cmps/note-img.cmp.js
@@ -2,8 +2,12 @@ export default {
     props:['cmp'],
     emits:['delete'],
     template:`
-    <h1>{{cmp.info.title}}</h1>
-    <button @click="onDeleteNote(cmp.id)">X</button>
+    <section class="img-container" @click="onEdit(cmp.id)">
+        <img :src="cmp.info.url" alt="">
+        <h1>{{cmp.info.title}}</h1>
+        <h2>{{cmp.info.txt}}</h2>
+        <button @click="onDeleteNote(cmp.id)">X</button>
+    </section>
     `,
     data(){
         return {
@@ -13,6 +17,9 @@ export default {
     methods:{
         onDeleteNote(id) {
             this.$emit('delete', id)
+        },
+        onEdit(id){
+            this.$emit('edit', id)
         }
     }
 }
