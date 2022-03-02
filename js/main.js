de0cccfb9f81c8916caa3198dd25e0de8cce26c4
Ron Shitrit <ronshitrit456@gmail.com>
2022-03-02 15:53:31 +0200

    test

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-import {router} from './router.js'
-import appHeader from './cmps/app-header.cmp.js';
-import appFooter from './cmps/app-footer.cmp.js';
+import {router} from '/router.js'
+import appHeader from '/cmps/app-header.cmp.js';
+import appFooter from '/cmps/app-footer.cmp.js';
 
 const options = {
     template: `
--- js/router.js
+++ js/router.js
@@ -1,7 +1,7 @@
-import keepApp from './apps/keep/pages/keep-app.cmp.js'
-import mailApp from './apps/mail/pages/mail-app.cmp.js'
-import homePage from './pages/home-page.cmp.js'
-import mailDetails from './apps/mail/pages/mail-details.cmp.js'
+import keepApp from '/apps/keep/pages/keep-app.cmp.js'
+import mailApp from '/apps/mail/pages/mail-app.cmp.js'
+import homePage from '/pages/home-page.cmp.js'
+import mailDetails from '/apps/mail/pages/mail-details.cmp.js'
 
 const routes = [
     {
